5c07918ab26bb01117485ce111236fd6a2a53755
Nilotpal Das <neelot3pal@gmail.com>
2021-08-24 00:22:58 +0530

    unauthenticated api used for deleteing posts

--- controllers/api/v1/posts_api.js
+++ controllers/api/v1/posts_api.js
@@ -1,6 +1,45 @@
-module.exports.index=function(req,res){
+const Post = require('../../../models/post');
+const Comment = require('../../../models/comment');
+module.exports.index= async function(req,res){
+    let posts =await Post.find({})
+    .populate('user')
+    .populate({
+        path:'comments',
+        populate : {
+            path: 'user'
+        }
+    });  
     return res.json(200,{
         message: "List of posts",
-        posts: []
+        posts: posts
     })
+}
+
+module.exports.destroy = async function(req,res){
+    try{
+        let post= await Post.findById(req.params.id);
+
+        // if(post.user == req.user.id){
+            post.remove();
+            
+            await Comment.deleteMany({post:req.params.id});
+            // req.flash('success','Deleted successfully');
+            
+            return res.status(200).json({
+                message: 'Deleted successfully'
+            });
+        
+    
+        
+    }
+    catch(err)
+    {
+        console.log(err);
+       return res.json(500,{
+
+            message: 'internal server error'
+       });
+    };
+   
+   
 }
--- routes/api/v1/posts.js
+++ routes/api/v1/posts.js
@@ -3,4 +3,5 @@ const router =express.Router();
 // router.use('/v1',require('./v1'));
 const postsApi = require('../../../controllers/api/v1/posts_api');
 router.get('/',postsApi.index);
+router.delete('/:id',postsApi.destroy);
 module.exports = router;
